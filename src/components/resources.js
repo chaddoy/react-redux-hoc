b488bab65bd167f1b44dc272193a0ba3e440e3d1
Chad Dumadag <capdumadag@gmail.com>
2017-02-15 15:44:48 +0800

    first commit

---
+++ src/components/resources.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react'
+
+class Resources extends React.Component {
+	render () {
+		return (
+			<div>
+				Super Special Secret Recipe
+
+				<ul>
+					<li>1 Cup Sugar</li>
+					<li>1 Cup Pepper</li>
+					<li>1 Cup Salt</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Resources;
